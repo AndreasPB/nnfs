73402ee4564e0d1f89f81a62ca6e5cb08f1850e4
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2022-01-29 16:14:45 +0100

    Finish p1 and p2 + WIP p3

---
+++ p3.py
@@ -0,0 +1,22 @@
+# %% [markdown]
+# # Part 3 - The Dot Product
+# https://www.youtube.com/watch?v=lGLto9Xd7bU&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3&index=2
+# %%
+inputs = [1, 2, 3, 2.5]
+
+weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+# %%
+# Noob way
+layer_outputs = []
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input * weight
+    neuron_output += neuron_bias
+    layer_outputs.append(neuron_output)
+
+layer_outputs
+
+# %%
